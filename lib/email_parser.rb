0bea496fa738f4a1b5fa96f6b5630ce0dc7ad252
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-13 23:09:43 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,3 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+class EmailAddressParser
+  attr_reader :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    emails.split(/, | /).uniq
+  end
+end
